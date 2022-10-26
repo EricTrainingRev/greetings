730ddc56e9b0f348b96749f1b2bdf105d72f16ef
EricTrainingRev <eric.suminski@revature.com>
2022-10-26 11:49:49 -0400

    updated english controller and deploy config

--- english/src/main/java/com/revature/english/GreetingController.java
+++ english/src/main/java/com/revature/english/GreetingController.java
@@ -3,16 +3,30 @@ package com.revature.english;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class GreetingController {
+
+    private static int count = 0;
+    private static double id= Math.random();
+
     @GetMapping("/english")
     public String genericGreeting(){
-        return "Hello there!";
+        count++;
+        return String.format("Hello there from container %s!",id);
     }
 
     @GetMapping("/english/{name}")
     public String personalGreeting(@PathVariable String name){
-        return "Hello "  + name + "!";
+        count++;
+        return "Hello "  + name + "from container " + id + "!" ;
     }
+
+    @GetMapping("/count")
+    public String returnCount() {
+        return "this app has recieved " + count + " requests.";
+    }
+    
 }
